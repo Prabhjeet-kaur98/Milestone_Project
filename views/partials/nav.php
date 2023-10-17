aed143e54e74150c9b51ede964e35ae7dd61e1dd
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 19:49:54 -0400

    Create nav.php file in paritials folder

--- views/partials/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-        <ul><li><a href="index.php">Home</a></li>
-            <li><a href="discover.php">Discover</a></li>
-            <li><a href="booking.php">Booking</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="signin.php">Sign In</a></li>
-        </ul>
-    </nav>
