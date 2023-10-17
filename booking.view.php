a0c4f6d52cbe579ead8623d4d908968a23669a24
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 18:56:11 -0400

    Create booking.view.php
    Add copy content from index.php

---
+++ booking.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+<link rel="stylesheet"  href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Travel Paradise</h1>
+    </header>
+    <nav>
+        <ul> <li><a href="index.php">Home</a></li>
+            <li><a href="discover.php">Discover</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="signin.php">Sign In</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+
+        <h2>Welcome to Booking page</h2>
+        <p>Explore the world's most amazing destinations with us.</p><br>
+    </div>
+    <footer>
+    <div class="container">
+        <p>&copy; 2023 Travel Buddy</p>
+    </div>
+</footer>
+
+</body>
+</html>
