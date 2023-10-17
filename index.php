6d1ce09c0d5dfa394aaa59570e0df33912869bce
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 13:26:14 -0400

    Add html code with navbar in index.html
    Add navbar with links discover, booking, about and sign in page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Travel Paradise</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Discover</a></li>
+            <li><a href="#">Booking</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Sign In</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
