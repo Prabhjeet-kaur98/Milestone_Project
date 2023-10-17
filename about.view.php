6a0e86da935f19d78799fded2d777a35588e8156
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 18:19:58 -0400

    Create about.view.php file and copy content from index.view.php

---
+++ about.view.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+<link rel="stylesheet"  href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Travel Paradise</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Discover</a></li>
+            <li><a href="#">Booking</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Sign In</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+
+        <h2>Welcome to Travel Buddy</h2>
+        <p>Explore the world's most amazing destinations with us.</p><br>
+        <p>hello</p><br>
+        <p>hello</p><br>
+        <p>hello</p><br>
+        <p>hello</p><br>
+    </div>
+    <footer>
+    <div class="container">
+        <p>&copy; 2023 Travel Buddy</p>
+    </div>
+</footer>
+
+</body>
+</html>
