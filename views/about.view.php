884977a62b52ee427b046405420c51cd08e9f328
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 22:33:20 -0400

    Add content in about.view.php page

--- views/about.view.php
+++ views/about.view.php
@@ -3,8 +3,20 @@
     <?php require('partials/nav.php') ?>
     <div class="container">
 
-        <h2>Welcome to About page</h2>
-        <p>Explore the world's most amazing destinations with us.</p><br>
+        <h2>About us</h2>
+        <div class="info-box">
+    <p>Welcome to Travel Paradise, your gateway to unforgettable adventures around the world. We are a passionate team of travel enthusiasts dedicated to making your journeys truly remarkable. Our mission is to provide you with the best travel experiences, offering a diverse range of destinations and activities that cater to all types of travelers.
+    
+    With years of experience in the travel industry, we've carefully curated a selection of unique destinations and accommodations. Our expert guides are here to ensure your safety and enhance your understanding of the places you visit. We believe that travel has the power to broaden horizons, foster understanding, and create lasting memories.
+    
+    Whether you're seeking relaxation on a pristine beach, an adventure in the mountains, or a deep dive into the history and culture of a place, Travel Paradise is your trusted partner. We offer competitive prices, top-notch customer service, and a commitment to making your travel experience exceptional.
+    
+    Our dedicated team works tirelessly to create seamless and immersive travel experiences for our clients. We believe that every journey should be filled with excitement, cultural enrichment, and moments of pure joy. Whether you're embarking on a solo adventure, planning a family vacation, or organizing a group tour, Travel Paradise has the expertise and resources to make it happen.
+    
+    Our approach to travel is grounded in a deep respect for the natural world and the diverse cultures that call our destinations home. We're committed to sustainable and responsible travel practices that protect the environment and support local communities. Your journey with us isn't just about exploring new places; it's about contributing positively to the world we share.</p>
+</div>
+
+    
         
     </div>
     
