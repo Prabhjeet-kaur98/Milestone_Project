23e61bcbab280a017bce13afcea630a512717794
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 22:49:00 -0400

    Update booking.view.php by addidng content for booking form

--- views/booking.view.php
+++ views/booking.view.php
@@ -1,8 +1,22 @@
 <?php require('partials/head.php') ?>
     <?php require('partials/nav.php') ?>
-    <div class="container">
-
-        <h2>Welcome to Booking page</h2>
-        <p>Explore the world's most amazing destinations with us.</p><br>
+    div class="booking-container"> <!-- Updated class name -->
+        <h2>Booking</h2>
+        <form class="booking-form"> <!-- Updated class name -->
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required>
+            
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+            
+            <label for="destination">Destination</label>
+            <select id="destination" name="destination" required>
+                <option value="beach">Exotic Beach Getaway</option>
+                <option value="mountain">Mountain Adventure</option>
+                <option value="historical">Historical Tours</option>
+            </select>
+            
+            <button type="submit">Book Now</button>
+        </form>
     </div>
     <?php require('partials/footer.php') ?>
