db5faaf3d59993296387fb1def6aa132b74b5c24
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 21:24:25 -0400

    Add padding to navbar

--- views/index.view.php
+++ views/index.view.php
@@ -4,6 +4,7 @@
 
         <h2>Welcome to Travel Buddy</h2>
         <p>Explore the world's most amazing destinations with us.</p><br>
+        <img src="images/travel.jpg" alt="Welcome to Travel Paradise" class="large-image">
     </div>
 
     <?php require('partials/footer.php') ?>
