a6a985497d10d6fd2b43de8bf0bdfa9a6549bdd8
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 21:54:57 -0400

    Add content with h2 heading and paragraph in discover.view.php

--- views/discover.view.php
+++ views/discover.view.php
@@ -2,8 +2,16 @@
     <?php require('partials/nav.php') ?>
     <div class="container">
 
-        <h2>Welcome to Discover page</h2>
-        <p>Explore the world's most amazing destinations with us.</p><br>
+    <h2>Discover</h2>
+    <p>Explore our curated list of destinations and find the perfect getaway for your next adventure. Whether you're looking for a tropical paradise, an urban exploration, or a tranquil retreat, Travel Paradise has something for every traveler.</p>
+    
+    <h3>Our Services</h3>
+    <ul>
+        <li>Discover unique and breathtaking destinations</li>
+        <li>Book your dream vacation with ease</li>
+        <li>Learn more about the places you'll visit</li>
+    </ul>
+
     </div>
     
     <?php require('partials/footer.php') ?>
