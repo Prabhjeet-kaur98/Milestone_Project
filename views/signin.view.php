78c1c57fa89aee3d905de4184030a9cefe50fe44
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-10-17 22:43:42 -0400

    Add style for sign in page form

--- views/signin.view.php
+++ views/signin.view.php
@@ -1,10 +1,15 @@
 <?php require('partials/head.php') ?>
     <?php require('partials/nav.php') ?>
-    <div class="container">
-
-        <h2>Welcome to Sign In page</h2>
-        <p>Explore the world's most amazing destinations with us.</p><br>
-    </div>
-    
+    <div class="sign-in-container"> <!-- Updated class name -->
+        <form>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+            
+            <button type="submit">Sign In</button>
+        </form>
+</div>
 
     <?php require('partials/footer.php') ?>
