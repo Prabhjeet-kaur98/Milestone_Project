d01b3d8a7731eac3a9d0cbe8e3e81e58e0bc83c9
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-11-08 00:39:16 -0500

    Add user registration functionality in PHP

--- views/signin.view.php
+++ views/signin.view.php
@@ -1,14 +1,45 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		if(!empty($username) && !empty($password) && !is_numeric($username))
+		{
+
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,username,password) values ('$user_id','$username','$password')";
+
+			mysqli_query($con, $query);
+
+			// header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
+
 <?php require('partials/head.php') ?>
     <?php require('partials/nav.php') ?>
     <div class="sign-in-container">
    <form>
             <label for="username">Username</label>
-            <input type="text" id="username" name="username" required>
-            
+            <input type="text" id="username" name="username">
+             
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password">
             
-            <button type="submit">Sign In</button>
+            <a class="button" href="index.php">Sign In</a><br><br>
         </form>
 </div>
 
