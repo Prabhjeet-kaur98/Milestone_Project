50449e3847abc235eacbcdeda21b57f33cc87111
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-12-05 16:07:57 -0500

    Remove error of word co on every page

--- views/signin.view.php
+++ views/signin.view.php
@@ -1,19 +1,19 @@
-<?php 
 <?php require('partials/head.php') ?>
-    <?php require('partials/nav.php') ?>
-    <h2>Signup</h2>
-    <form action="signup_process.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+<?php require('partials/nav.php') ?>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" required><br>
+<h2>Signup</h2>
+<form action="signup_process.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required><br>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+    <label for="email">Email:</label>
+    <input type="email" name="email" required><br>
 
-        <button type="submit">Signup</button>
-    </form>
-</div>
+    <label for="password">Password:</label>
+    <input type="password" name="password" required><br>
 
-    <?php require('partials/footer.php') ?>
+    <button type="submit">Signup</button>
+</form>
+
+<?php require('partials/footer.php') ?>
+        
