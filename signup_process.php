0ceeef623a5c3724224a998e04b1dedb961cfd6d
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-12-05 16:11:11 -0500

    Add new code to signup_process.php page

--- signup_process.php
+++ signup_process.php
@@ -1,17 +1,18 @@
 <?php
-// Database connection
 $db = new mysqli('localhost', 'root', '', 'travel_paradise');
 
 // Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
+} else {
+    echo "Connected successfully"; 
 }
 
 // Process form data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
 
     // Insert data into the database
     $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
@@ -21,6 +22,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "Error: " . $sql . "<br>" . $db->error;
     }
+} else {
+    echo "No POST data received"; 
 }
 
 $db->close();
