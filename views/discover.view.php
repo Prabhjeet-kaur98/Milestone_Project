c676432e468e2051950ecd0a0525cb57e20feb49
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-12-05 20:55:51 -0500

    change background color hover for nav and padding to footer

--- views/discover.view.php
+++ views/discover.view.php
@@ -6,7 +6,7 @@
     <p>Explore our curated list of destinations and find the perfect getaway for your next adventure. Whether you're looking for a tropical paradise, an urban exploration, or a tranquil retreat, Travel Paradise has something for every traveler.</p>
     
     <h3>Our Services</h3>
-    <ul>
+    <ul class="services_page">
         <li>Discover unique and breathtaking destinations</li>
         <li>Book your dream vacation with ease</li>
         <li>Learn more about the places you'll visit</li>
